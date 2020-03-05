d4c223dab09d7010ce7a796c3e782582e88ad6cd
Michael Pidgeon <pidge@Nicolas-MacBook.local>
2020-03-05 14:23:13 +0000

    wip

--- backend/app.js
+++ backend/app.js
@@ -5,17 +5,25 @@ const app = new Koa();
 var Router = require('koa-router');
 var router = new Router();
 
-const parseData = require('./parser.js').parseData;
-
-const setData = results => (app.context.data = results);
-parseData(setData);
+const getCsvData = require('./parser.js').getCsvData;
 
 router.get('/medals', async (ctx, next) => {
+  const data = await getCsvData();
   const limit = Number(ctx.request.query.limit || 10);
   const offset = Number(ctx.request.query.offset || 0);
+  const eventFilter = ctx.request.query.event;
+  const yearFilter = ctx.request.query.year;
+  const nameFilter = ctx.request.query.name;
+  let filteredData = [];
+  data.forEach(m => {
+    if (eventFilter && eventFilter !== m.Event) return;
+    if (yearFilter && yearFilter !== m.Games) return;
+    if (nameFilter && nameFilter !== m.Name) return;
+    filteredData.push(m);
+  });
   const startPoint = limit * offset;
   const endPoint = startPoint + limit;
-  ctx.body = ctx.data.slice(startPoint, endPoint);
+  ctx.body = filteredData.slice(startPoint, endPoint);
 });
 
 app
@@ -23,4 +31,6 @@ app
   .use(router.routes())
   .use(router.allowedMethods());
 
-app.listen(4000);
+const server = app.listen(4000);
+
+exports.server = server;
